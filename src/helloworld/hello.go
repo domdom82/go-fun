804eb05b7eb104cef7b5a884b968df0e1fc4895d
Dominik Jall <djall@de.ibm.com>
2016-11-08 16:02:32 +0100

    made helloworld a bit fancier

--- src/helloworld/hello.go
+++ src/helloworld/hello.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-    "fmt"
-//    "os"
-//    "strings"
+	"fmt"
+	"os"
+	"strings"
 )
 
+// This is my first ever go program. Excited to learn a new language!
 func main() {
-    fmt.Println("Hello World!")
+	name := "World"
+
+	// If a name was passed, we greet that name instead.
+	if len(os.Args) > 0 {
+		name = strings.Join(os.Args[1:], " ")
+	}
+
+	fmt.Printf("Hello %s!\n", name)
 }
